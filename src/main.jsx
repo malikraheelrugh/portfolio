2616456fad7c6958ebcaf8911a9348c5064de4f9
malikraheelrugh <raheelrugh677@gmail.com>
2025-09-16 15:24:01 +0500

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -3,24 +3,21 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Contact from './Contact.jsx'
 import App from './App.jsx'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import MyProjects from './components/myproject.jsx'
 BrowserRouter
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-  <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<App />}>
-      </Route>
-    </Routes>
-    <Routes>
-      <Route path='/myprojects' element={<MyProjects />}>
-      </Route>
-    </Routes>
-    <Routes>
-      <Route path='/contact' element={<Contact />}>
-      </Route>
-    </Routes>
-  </BrowserRouter>
-  // </StrictMode>,
+  <StrictMode>
+    <BrowserRouter>
+
+      <Routes>
+        <Route path='/' element={<App />}>
+        </Route>
+        <Route path='/myprojects' element={<MyProjects />}>
+        </Route>
+        <Route path='/contact' element={<Contact />}>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
